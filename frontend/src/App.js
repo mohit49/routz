fb267dfaf181c1607bc2b3aaae55cb1d1cc29792
aman62001 <mohitanim@gmail.com>
2022-10-02 01:47:09 +0530

    live add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, createContext } from "react";
 import ReactDOM from 'react-dom';
 import { Routes, Route, Link } from "react-router-dom";
 
-import Header from "../includes/Header/Header";
-import Login from "../Pages/Login/Login";
-import Register from "../pages/Register/Register";
-import Profile from "../Pages/Profile/Profile";
+import Header from "./includes/Header/Header";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Profile from "./Pages/Profile/Profile";
 export const Data = createContext();
 export function App() {
   const [loginState, setLoginState] = useState(false);
