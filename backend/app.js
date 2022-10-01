7a664c98bf9c1581d16af7c0739a5bcaaf60462c
aman62001 <mohitanim@gmail.com>
2022-10-01 23:59:04 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,26 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-  
-// Create Express Server
-const app = express();
-  
-// Configuration
-const PORT = 3003;
-const HOST = "localhost";
+var http = require('http');
 
-  
+http.createServer(onRequest).listen(80);
 
-  
-// Proxy Logic :  Proxy endpoints
-app.use(
-    "/api",
-    createProxyMiddleware({
-        target: "http://82.180.137.231:8080",
-        changeOrigin: true,
-        pathRewrite: {
-            "^/api": "",
-        },
-    })
-);
-  
-// Starting our Proxy server
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+function onRequest(client_req, client_res) {
+  console.log('serve: ' + client_req.url);
+
+  var options = {
+    hostname: '82.180.137.231',
+    port: 3001,
+    path: client_req.url,
+    method: client_req.method,
+    headers: client_req.headers
+  };
+
+  var proxy = http.request(options, function (res) {
+    client_res.writeHead(res.statusCode, res.headers)
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
