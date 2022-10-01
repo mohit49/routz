302e6066e15d380894faa5e835ebec58d2c92ab2
aman62001 <mohitanim@gmail.com>
2022-10-01 23:19:19 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,29 @@
 const express = require("express");
-const app = express();
-const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-require("./db/db");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const http = require("http");
-const cors = require("cors");
-const https = require("https");
+  
+// Create Express Server
+const app = express();
+  
+// Configuration
+const PORT = 3001;
+const HOST = "localhost";
 
-const path = require("path");
-//const bodyParser = require('body-parser');
+  
 
-const httpProxy = require('http-proxy');
-
-httpProxy.createProxyServer({target:'http://82.180.137.231:3001'}).listen(80);
+  
+// Proxy Logic :  Proxy endpoints
 app.use(
-  cors({
-    origin: "http://localhost:8080",
-    credentials: true,
-  })
+    "/weather",
+    createProxyMiddleware({
+        target: "http://82.180.137.231:8080/api",
+        changeOrigin: true,
+        pathRewrite: {
+            "^/weather": "",
+        },
+    })
 );
-
-app.use(express.json({ limit: "50mb" }));
-//app.use(express.urlencoded({limit: '50mb'}));
-app.use(cookieParser());
-
-app.use((req, res, next) => {
-  console.log(req.method + req.url);
-  next();
+  
+// Starting our Proxy server
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
-
-/**
- * Api require Modules Name
- * @type {string}
- */
-
-const registerPage = require("./modules/registration/registration");
-app.post("/register", registerPage);
-
-const loginPage = require("./modules/login/login");
-app.post("/login", loginPage);
-
-const users = require("./modules/users/users");
-app.get("/users", users);
-const editprofile = require("./modules/editprofile/editprofile");
-app.post("/editprofile", editprofile);
-
-const followers = require("./modules/follow/follow");
-app.post("/follow", followers);
-const unfollow = require("./modules/follow/unfollow");
-app.post("/unfollow", unfollow);
-const getProfile = require("./modules/editprofile/fetchprofile");
-app.get("/profile", getProfile);
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(3001);
