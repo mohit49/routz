5a175fd3a7714b390235e8cc76a744cf3fa7f263
aman62001 <mohitanim@gmail.com>
2022-10-05 23:58:33 +0530

    live add

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017',{
+mongoose.connect('mongodb://127.0.0.1:27017/routz',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
    
