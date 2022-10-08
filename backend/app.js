c83ab6ab9e591dc34278ae825307c401f902b7d0
aman62001 <mohitanim@gmail.com>
2022-10-09 02:54:03 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
