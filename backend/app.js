30dc5cf03c33b77760bc8fbf0130795892e19ea4
aman62001 <mohitanim@gmail.com>
2022-10-09 03:19:59 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,9 @@ const http = require("http").Server(app);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log(req.method + req.url);
-  next();
-});
+
+
 
 /**
  * Api require Modules Name
