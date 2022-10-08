89a0bb987aa61ea703851a3d89a5cd01ec5e0eaf
aman62001 <mohitanim@gmail.com>
2022-10-09 03:36:48 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
 app.post('/register',function(req,res){
   console.log(req.body)
 });
