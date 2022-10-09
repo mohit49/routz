2fdbfd1aca0e1eebee1f5dd24bd742a75fb8b359
aman62001 <mohitanim@gmail.com>
2022-10-09 11:19:28 +0530

    live add

--- backend/scemas/registration.js
+++ backend/scemas/registration.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose');
-console.log('sss')
+
 const users = new mongoose.Schema({
     name : {
         type:String,
-      
+        required:true
     },
     username:{
         type:String,
-        
+        required:true
     },
     email : {
         type:String,
-        
+        required:true
     },
     companyname : {
         type:String,
-      
+        required:true
     },
     phone : {
         type:String,
-      
+        required:true
     },
     usertype : {
         type:String,
-       
+        required:true
     },
     password : {
         type:String,
-      
+        required:true
     }
 
 })
-console.log('sdd')
+
 const Register = new mongoose.model("Registration",users);
 module.exports = Register;
