f2f9d84a38acadba07b3a094d5f5e094abe4fdf2
aman62001 <mohitanim@gmail.com>
2022-10-09 11:17:53 +0530

    live add

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,55 @@
 const express = require("express");
 const app = express();
-const http = require("http").Server(app);
-app.use(express.json({limit: '50mb'}));
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+require("./db/db");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-app.post('/register',function(req,res){
-  console.log(req.body)
+const cors = require("cors");
+const https = require("https");
+
+const path = require("path");
+//const bodyParser = require('body-parser');
+const http = require("http").Server(app);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+//app.use(express.urlencoded({limit: '50mb'}));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log(req.method + req.url);
+  next();
 });
 
+/**
+ * Api require Modules Name
+ * @type {string}
+ */
 
+const registerPage = require("./modules/registration/registration");
+app.post("/register", registerPage);
 
-http.listen(3004, function() {
+const loginPage = require("./modules/login/login");
+app.post("/login", loginPage);
+
+const users = require("./modules/users/users");
+app.get("/users", users);
+const editprofile = require("./modules/editprofile/editprofile");
+app.post("/editprofile", editprofile);
+
+const followers = require("./modules/follow/follow");
+app.post("/follow", followers);
+const unfollow = require("./modules/follow/unfollow");
+app.post("/unfollow", unfollow);
+const getProfile = require("./modules/editprofile/fetchprofile");
+app.get("/profile", getProfile);
+http.listen(3004, function () {
   console.log("listening on *:4000");
 });
