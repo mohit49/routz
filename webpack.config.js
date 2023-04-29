eb75267de6ce9877c78940fddf9a2ff021717498
mohit49 <mohitanim@gmail.com>
2023-04-29 12:48:14 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -105,19 +105,7 @@ module.exports =  {
     },
     optimization: {
        minimize: true,
-      minimizer: [new UglifyJsPlugin({
-        uglifyOptions: {
-          warnings: false,
-          parse: {},
-          compress: {},
-          mangle: true, // Note `mangle.properties` is `false` by default.
-          output: null,
-          toplevel: false,
-          nameCache: null,
-          extractComments: true,
-          ie8: false,
-          keep_fnames: false,}
-      })],
+      minimizer: [new UglifyJsPlugin()],
     },
   
 };
