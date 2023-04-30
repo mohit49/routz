fca28f4b00674f926620e9e4f18034f1a5383743
mohit49 <mohitanim@gmail.com>
2023-04-30 12:51:40 +0530

    changes

--- src/client/Pages/Home/Home.js
+++ src/client/Pages/Home/Home.js
@@ -80,11 +80,7 @@ function Home() {
           {
             //<Slider effect='fade' sliderName='homeSlider' autoPlay={true} items={sliderItem} dotPosition='bottom' buttonClass='primary' className='slider-home'/>
           }
-          <div className="banner-heading">
-          <LazyLoadComponent>
-            <h1>Searching For a Bike Ride to Ladhak ? </h1>
-            </LazyLoadComponent>
-          </div>
+          
           <SearchBar />
         </Container>
         <div className="staticimage">
