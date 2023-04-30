1dbc12eb45fae0dcdf486251c99be7ed2cd877b3
mohit49 <mohitanim@gmail.com>
2023-04-30 12:35:47 +0530

    change

--- src/client/Pages/Home/Home.js
+++ src/client/Pages/Home/Home.js
@@ -4,107 +4,113 @@ import axios from "axios";
 import { Spinner } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { Data } from "../../App";
-import Bgimg from '../../assets/images/web-main.jpg'
+import Bgimg from "../../assets/images/web-main.jpg";
 import "../Home/Home.scss";
 import Container from "react-bootstrap/Container";
 import Slider from "../../uiElements/Slider/Slider";
 import LatestEvents from "../../Component/LatestEvents/LatestEvents";
-import Banner1  from "../../assets/images/banner1.jpg";
-import Banner2  from "../../assets/images/banner2.jpg";
-import Banner3  from "../../assets/images/banner3.jpg";
-import Banner1_mob  from "../../assets/images/banner1_mob.jpg";
-import Banner2_mob  from "../../assets/images/banner2_mob.jpg";
-import Banner3_mob  from "../../assets/images/banner3_mob.jpg";
+import Banner1 from "../../assets/images/banner1.jpg";
+import Banner2 from "../../assets/images/banner2.jpg";
+import Banner3 from "../../assets/images/banner3.jpg";
+import Banner1_mob from "../../assets/images/banner1_mob.jpg";
+import Banner2_mob from "../../assets/images/banner2_mob.jpg";
+import Banner3_mob from "../../assets/images/banner3_mob.jpg";
 import SearchBar from "../../Component/SearchBar/SearchBar";
 import Latestposts from "../../Component/Latestposts/Latestposts";
 import Ridersupdate from "../../Component/Ridersupdate/Ridersupdate";
 import RidersFeed from "../../Component/RidersFeed/RidersFeed";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import About from "../../Component/About/About";
 import Announcement from "../../Component/Announcement/Announcement";
-import { LazyLoadComponent } from 'react-lazy-load-image-component';
+import { LazyLoadComponent } from "react-lazy-load-image-component";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 const profileFetch =
   process.env.REACT_BASE_API_URL + process.env.REACT_APP_PROFILE_FETCH;
 function Home() {
-  const [riderData , setRiderData] = useState(false);
+  const [riderData, setRiderData] = useState(false);
   const { loginState, setLoginState, profileData, setProfileData } =
     useContext(Data);
-const sliderItem = {
-    slide1:{
-        'image': Banner1,
-        'img_mob':Banner1_mob,
-        'cta': true,
-        'ctaLink':'#',
-        'ctaText':'Find Now',
-        'sliderHeading':'Find And event for riding',
-        'sliderText':`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
-        'className':'slider-con-inner',
-        'buttonVariant':'primary',
-        'btnSize':'md'
+  const sliderItem = {
+    slide1: {
+      image: Banner1,
+      img_mob: Banner1_mob,
+      cta: true,
+      ctaLink: "#",
+      ctaText: "Find Now",
+      sliderHeading: "Find And event for riding",
+      sliderText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
+      className: "slider-con-inner",
+      buttonVariant: "primary",
+      btnSize: "md",
     },
-    slide2:{
-        'image':Banner2,
-        'img_mob':Banner2_mob,
-        'cta': true,
-        'ctaLink':'#',
-        'ctaText':'Search Now',
-        'sliderHeading':'Search for a Rider',
-        'sliderText':`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
-        'className':'slider-con-inner',
-        'buttonVariant':'primary',
-        'btnSize':'md'
+    slide2: {
+      image: Banner2,
+      img_mob: Banner2_mob,
+      cta: true,
+      ctaLink: "#",
+      ctaText: "Search Now",
+      sliderHeading: "Search for a Rider",
+      sliderText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
+      className: "slider-con-inner",
+      buttonVariant: "primary",
+      btnSize: "md",
     },
-    slide3:{
-        'image':Banner3,
-        'img_mob':Banner3_mob,
-        'cta': true,
-        'ctaLink':'www.google.com',
-        'ctaText':'Search Now',
-        'sliderHeading':'Search for a Show Room',
-        'sliderText':`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
-        'className':'slider-con-inner',
-        'buttonVariant':'primary',
-        'btnSize':'md'
+    slide3: {
+      image: Banner3,
+      img_mob: Banner3_mob,
+      cta: true,
+      ctaLink: "www.google.com",
+      ctaText: "Search Now",
+      sliderHeading: "Search for a Show Room",
+      sliderText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`,
+      className: "slider-con-inner",
+      buttonVariant: "primary",
+      btnSize: "md",
     },
-}
+  };
   return (
     <>
-    <Helmet>
-    <title>Social Networks For bikers :: Home</title>
-    <meta name="description" content="Social Networks For bikers"/>
-  <meta name="keywords" content="HTML, CSS, JavaScript, Home"/>
-    </Helmet>
-    <div className="home-banner"  style={{ 
-      backgroundImage: `url(${Bgimg})` 
-    }}>
-    <Container className="simpleSection container-mob" fluid="md" >
-      {//<Slider effect='fade' sliderName='homeSlider' autoPlay={true} items={sliderItem} dotPosition='bottom' buttonClass='primary' className='slider-home'/>
-}
-<div className="banner-heading">
-<h1>Searching For a Bike Ride to Ladhak ? </h1>
-</div>
-<SearchBar/>
-    </Container>
-    </div>
-   
-     <Container className="bgChange event-sec" fluid>
-     <LazyLoadComponent>
-   <LatestEvents/>
-   </LazyLoadComponent>
-   </Container>
-   
-   <LazyLoadComponent>
-   <Latestposts/>
-  </LazyLoadComponent>
-  <About/>
-  <Announcement/>
-  <LazyLoadComponent>
+      <Helmet>
+        <title>Social Networks For bikers :: Home</title>
+        <meta name="description" content="Social Networks For bikers" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, Home" />
+      </Helmet>
+      <div className="home-banner">
+        <Container className="simpleSection container-mob" fluid="md">
+          {
+            //<Slider effect='fade' sliderName='homeSlider' autoPlay={true} items={sliderItem} dotPosition='bottom' buttonClass='primary' className='slider-home'/>
+          }
+          <div className="banner-heading">
+            <h1>Searching For a Bike Ride to Ladhak ? </h1>
+          </div>
+          <SearchBar />
+        </Container>
+        <div className="staticimage">
+          
+          <LazyLoadImage
+            effect="blur"
+            alt="example"
+            src={Bgimg}
+          />
+        </div>
+      </div>
 
-   {!riderData && <Ridersupdate riderData={setRiderData}/> }
-   </LazyLoadComponent>
-<RidersFeed/>
- 
-   </>
+      <Container className="bgChange event-sec" fluid>
+        <LazyLoadComponent>
+          <LatestEvents />
+        </LazyLoadComponent>
+      </Container>
+
+      <LazyLoadComponent>
+        <Latestposts />
+      </LazyLoadComponent>
+      <About />
+      <Announcement />
+      <LazyLoadComponent>
+        {!riderData && <Ridersupdate riderData={setRiderData} />}
+      </LazyLoadComponent>
+      <RidersFeed />
+    </>
   );
 }
 
