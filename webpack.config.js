6db5f68cbded940a34e650490a0b4525bb9d3654
mohit49 <mohitanim@gmail.com>
2023-04-30 09:26:29 +0530

    cdcd

--- webpack.config.js
+++ webpack.config.js
@@ -2,76 +2,79 @@ const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const Dotenv = require("dotenv-webpack");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const RemovePlugin = require('remove-files-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const outputDirectory = 'server-build';
-const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production';
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const RemovePlugin = require("remove-files-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const outputDirectory = "server-build";
+const isProduction =
+  process.argv[process.argv.indexOf("--mode") + 1] === "production";
 
-module.exports =  {
-  entry: [ './src/client/index.js'],
-  target: ["web", 'es5'],
+module.exports = {
+  entry: ["./src/client/index.js"],
+  target: ["web", "es5"],
 
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'index.min.js'
+    filename: "index.min.js",
   },
   devServer: {
     port: 3000,
     open: true,
     historyApiFallback: true,
     proxy: {
-      '/api': 'http://localhost:3004'
-    }
-  }
-,
+      "/api": "http://localhost:3004",
+    },
+  },
   module: {
     rules: [
-      
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-react'],
+          presets: ["@babel/preset-react"],
         },
       },
       {
         test: /\.scss$/,
-        
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader" ],
+
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
 
       {
         test: /\.less$/,
-        
-        use: [{
-          loader: "style-loader"
-      }, {
-          loader: "css-loader"
-      }, {
-          loader: "less-loader",
-          options: {
-            lessOptions: {
-               javascriptEnabled: true
-            }
-          }
-      }]
+
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
               query: {
-                name:'assets/images/[name].[ext]'
-              }
-            }
+                name: "assets/images/[name].[ext]",
+              },
+            },
           },
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             options: {
               query: {
                 mozjpeg: {
@@ -82,31 +85,35 @@ module.exports =  {
                 },
                 optipng: {
                   optimizationLevel: 7,
-                }
-              }
-            }
-          }]
-      }
+                },
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
-   
+    extensions: ["*", ".js", ".jsx"],
   },
-  plugins: [new CleanWebpackPlugin([outputDirectory]), new MiniCssExtractPlugin() , new Dotenv({path :`${isProduction ? '.env.production' : '.env.development' }`}),
-  new HtmlWebpackPlugin({
-    template: './public/index.html',
-    favicon: './public/favicon.ico'
-  })],
-   performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    },
-    optimization: {
-       minimize: isProduction ? true : false,
-       
-      minimizer: [new UglifyJsPlugin()],
-    },
-  
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new MiniCssExtractPlugin(),
+    new Dotenv({
+      path: `${isProduction ? ".env.production" : ".env.development"}`,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  optimization: {
+    minimize: isProduction ? true : false,
+    minimizer: [new UglifyJsPlugin()],
+  },
 };
