8b331c3da1fa2a2043b7db0b62651517ddf67831
mohit49 <mohitanim@gmail.com>
2023-05-01 16:14:04 +0530

    changes commit

--- src/server/modules/search-bar/search-bar.js
+++ src/server/modules/search-bar/search-bar.js
@@ -14,199 +14,75 @@ router.get("/api/search-bar", async (req, res) => {
   const query = req.query?.query?.toLowerCase() || "";
   const state = req.query?.state || "";
   const city = req.query?.city || "";
+  if (searchtype?.length > 0) {
+    var searchArray = { resultBikers: {}, resultEvents: {} };
+    const searchArr = searchtype.split(",");
+    var profileSearch, eventSearch;
 
-  var searchArray = { resultBikers: {}, resultEvents: {} };
-if(state.length > 0 && city.length > 0) {
-   var profileSearch = await RegisterData.aggregate([
-      {
-        $match: {
-          usertype: "biker",
-          $and: [
-            {
-              $or: [
-                { name: { $regex: new RegExp(query, "i") } },
-                { username: { $regex: new RegExp(query, "i") } },
-             
+    for await (const ele of searchArr) {
+      if ((await ele) == "biker") {
+        profileSearch = await RegisterData.aggregate([
+          {
+            $match: {
+              usertype: "biker",
+              $and: [
+                {
+                  $or: [
+                    { name: { $regex: new RegExp(query, "i") } },
+                    { username: { $regex: new RegExp(query, "i") } },
+                  ],
+                },
               ],
             },
-          ],
-        },
-      },
-      { $skip: queryIndex ? queryIndex : 0},
-  
-      { $limit : limit ? limit : 0 },
-  
-      {
-        $lookup: {
-          from: `profileinfos`,
-          localField: "username",
-          foreignField: "username",
-          as: "more",
-  
-        },
-        
-      },
-      { $match : {
-        $and: [{$or:[{ 'more.location.state': state }],
-              }]}
-       },
-       { $match : {
-        $and: [{$or:[{ 'more.location.city': city }],
-              }]}
-       },
-      { $unset: "password" }
-    ]);
-  } else if(state.length > 0){
-   
-    var profileSearch = await RegisterData.aggregate([
-      {
-        $match: {
-          usertype: "biker",
-          $and: [
-            {
-              $or: [
-                { name: { $regex: new RegExp(query, "i") } },
-                { username: { $regex: new RegExp(query, "i") } },
-             
-              ],
-            },
-          ],
-        },
-      },
-      { $skip: queryIndex ? queryIndex : 0},
-  
-      { $limit : limit ? limit : 0 },
-  
-      {
-        $lookup: {
-          from: `profileinfos`,
-          localField: "username",
-          foreignField: "username",
-          as: "more",
-  
-        },
-        
-      },
-      { $match : {
-        $and: [{$or:[{ 'more.location.state': state }],
-              }]}
-       },
-      
-      { $unset: "password" }
-    ]);
-  } else if(city.length > 0) {
-    var profileSearch = await RegisterData.aggregate([
-      {
-        $match: {
-          usertype: "biker",
-          $and: [
-            {
-              $or: [
-                { name: { $regex: new RegExp(query, "i") } },
-                { username: { $regex: new RegExp(query, "i") } },
-             
-              ],
-            },
-          ],
-        },
-      },
-    
-  
-      {
-        $lookup: {
-          from: `profileinfos`,
-          localField: "username",
-          foreignField: "username",
-          as: "more",
-  
-        },
-        
-      },
-     
-       { $match : {
-        $and: [{$or:[{ 'more.location.city': city }],
-              }]}
-       },
-       { $skip: queryIndex ? queryIndex : 0},
-  
-       { $limit : limit ? limit : 0 },
-      { $unset: "password" }
-    ]);
-  }
-  
-  else {
-    var profileSearch = await RegisterData.aggregate([
-      {
-        $match: {
-          usertype: "biker",
-          $and: [
-            {
-              $or: [
-                { name: { $regex: new RegExp(query, "i") } },
-                { username: { $regex: new RegExp(query, "i") } },
-             
-              ],
-            },
-          ],
-        },
-      },
-      { $skip: queryIndex ? queryIndex : 0},
-  
-      { $limit : limit ? limit : 0 },
-  
-      {
-        $lookup: {
-          from: `profileinfos`,
-          localField: "username",
-          foreignField: "username",
-          as: "more",
-  
-        },
-        
-      },
-      
-      { $unset: "password" }
-    ]);
-  }
+          },
+          { $skip: queryIndex ? queryIndex : 0 },
 
-  var eventResultSearch = await Createevent.find({
-    $and: [
-      {
-        $or: [
-          { eventtitle: { $regex: new RegExp(query) } },
-          { creatorname: { $regex: new RegExp(query) } },
-        ],
-      },
-    ],
-  })
-    .limit(limit)
-    .skip(queryIndex);
-  if (searchtype?.includes(",")) {
-    var searchTypeArray = searchtype.split(",");
-    if (searchTypeArray.indexOf("biker") > -1) {
-      searchArray.resultBikers = profileSearch;
-    }
-    if (searchTypeArray.indexOf("event") > -1) {
-      searchArray.resultEvents = eventResultSearch;
-    }
-   
-  } else if (searchtype?.length > 0) {
+          { $limit: limit ? limit : 0 },
 
-    if (searchtype == "biker") {
-      searchArray.resultBikers = profileSearch;
-   
+          {
+            $lookup: {
+              from: `profileinfos`,
+              localField: "username",
+              foreignField: "username",
+              as: "more",
+            },
+          },
+
+          { $unset: "password" },
+        ]);
+        searchArray.resultBikers = profileSearch;
+        res.status(200).json({
+          sucessStatus: true,
+          data: searchArray,
+        });
+      } else if (ele == "event") {
+        var eventResultSearch = await Createevent.find({
+          $and: [
+            {
+              $or: [
+                { eventtitle: { $regex: new RegExp(query , "i") } },
+                { creatorname: { $regex: new RegExp(query , "i") } },
+                { "state.name": { $regex: new RegExp(query , "i") } },
+                { "city.name": { $regex: new RegExp(query , "i") } },
+              ],
+            },
+          ],
+        })
+          .limit(limit)
+          .skip(queryIndex);
+          searchArray.eventSearch = eventResultSearch;
+          res.status(200).json({
+            sucessStatus: true,
+            data: searchArray,
+          });
+      }
     }
-    if (searchtype == "event") {
-      searchArray.resultEvents = eventResultSearch;
-    }
- 
   } else {
-
+    res.status(200).json({
+      sucessStatus: false,
+      data: { message: "Error occured bacause Search type not included" },
+    });
   }
-  res.status(200).json({
-    sucessStatus: true,
-    data: searchArray,
-  });
 });
 
 module.exports = router;
